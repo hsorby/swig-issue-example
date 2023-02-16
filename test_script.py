4cb0a1fcc86a60e7f7d7062aed006b44a956fd07
Hugh Sorby <h.sorby@auckland.ac.nz>
2023-02-16 20:52:07 +1300

    Update test_script.py to not use createShared().

--- test_script.py
+++ test_script.py
@@ -5,11 +5,12 @@ from baz import Baz
 
 b1 = Bar()
 b2 = Baz()
-b_shared = Baz.createShared()
+#b_shared = Baz.createShared()
 
 b1.setX(7)
 b2.setX(9)
-b_shared.setX(13)
+#b_shared.setX(13)
 
-print(f"Bar value is: {b1.x()}, Baz value is: {b2.x()}, Baz shared value is: {b_shared.x()}.")
+#print(f"Bar value is: {b1.x()}, Baz value is: {b2.x()}, Baz shared value is: {b_shared.x()}.")
+print(f"Bar value is: {b1.x()}, Baz value is: {b2.x()}.")
 
